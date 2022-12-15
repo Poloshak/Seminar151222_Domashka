3b61b47ea4737f33aa76d2694a54a6abcc25fc83
Poloshak <e.l.fomenko@yandex.ru>
2022-12-16 02:06:42 +0300

    new

---
+++ Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{m};{n} -> {SumNumbers(m,n)}");
+
+int SumNumbers(int number1, int number2)
+{
+    if (number1 == number2)
+        return number1;
+    return (number1 + SumNumbers(number1+1,number2));
+}
---
+++ Task68/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+Console.Write("Введите неотрицательное число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите неотрицательное число N: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{m};{n} -> A(m,n) = {Ack(m,n)}");
+
+int Ack(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if ((m > 0) && (n == 0)) return Ack(m - 1, 1);
+    return Ack(m - 1, Ack(m, n - 1));
+}
